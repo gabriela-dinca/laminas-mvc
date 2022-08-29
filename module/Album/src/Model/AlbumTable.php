9f37970f2312b155b246ca96f5144cebebb8de42
Gabi <gabriela.dinca@codenground.de>
2022-08-29 10:02:15 +0200

    Initial commit

---
+++ module/Album/src/Model/AlbumTable.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace Album\Model;
+
+use Laminas\Db\ResultSet\ResultSetInterface;
+use RuntimeException;
+use Laminas\Db\TableGateway\TableGatewayInterface;
+
+class AlbumTable
+{
+    private TableGatewayInterface $tableGateway;
+
+    public function __construct(TableGatewayInterface $tableGateway)
+    {
+        $this->tableGateway = $tableGateway;
+    }
+
+    public function fetchAll(): ResultSetInterface
+    {
+        return $this->tableGateway->select();
+    }
+
+    public function getAlbum($id): Album
+    {
+        $id = (int) $id;
+        $rowSet = $this->tableGateway->select(['id' => $id]);
+        $row = $rowSet->current();
+        if (! $row) {
+            throw new RuntimeException(sprintf(
+                'Could not find row with identifier %d',
+                $id
+            ));
+        }
+
+        return $row;
+    }
+
+    public function saveAlbum(Album $album): void
+    {
+        $data = [
+            'artist' => $album->artist,
+            'title'  => $album->title,
+        ];
+
+        $id = (int)$album->id;
+
+        if ($id === 0) {
+            $this->tableGateway->insert($data);
+            return;
+        }
+
+        try {
+            $this->getAlbum($id);
+        } catch (RuntimeException $e) {
+            throw new RuntimeException(sprintf(
+                'Cannot update album with identifier %d; does not exist',
+                $id
+            ));
+        }
+
+        $this->tableGateway->update($data, ['id' => $id]);
+    }
+
+    public function deleteAlbum($id): void
+    {
+        $this->tableGateway->delete(['id' => (int) $id]);
+    }
+}
